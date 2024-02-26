0645c35ea80352640db388061833e11952c6ccec
Shishira-bayari <110318753+Shishira-bayari@users.noreply.github.com>
2024-02-26 15:37:49 +0530

    Add files via upload

---
+++ components/About.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 1.5, 1.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-17 h-18 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>what is BitsNBytes.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Welcome to the official website of BitsNbytes, 
+        your gateway to the exciting world of technology and innovation! 
+        We are a dynamic technical club operating under the esteemed Information Science Department, 
+        dedicated to fostering a vibrant community of tech enthusiasts, learners, and creators.
+      </motion.p>
+
+      <motion.div variants={textVariant()}>
+        
+        <h2 className={styles.sectionHeadText}>What we Do</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        BitsNbytes is more than just a club; 
+        it's a hub for innovation, learning, and creativity. 
+        Our aim is to connect like-minded individuals who are 
+        passionate about technology and are eager to explore 
+        and excel in the ever-evolving tech landscape. 
+      </motion.p><br/>
+
+      <br /><motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Core Members Of</p>
+        <h2 className={styles.sectionHeadText}>BitsNBytes.</h2>
+      </motion.div>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ constants/index.js
@@ -0,0 +1,253 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    
+    shopify,
+    carrent,
+    
+    tripguide,
+    threejs,
+    Game,
+    Deeps,
+    sbayari,
+    Jh,
+    Ananya,
+    MahaL,
+    G1,
+    Uj,
+    Prer,
+    Venki,
+    WeBD,
+    Sattu,
+    BNB,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "F.R.I.D.A.Y",
+      icon: Deeps,
+    },
+    {
+      title: "Captain Code",
+      icon: sbayari,
+    },
+    {
+      title: "Maverick",
+      icon: Jh,
+    },
+    {
+      title: "Maverick",
+      icon: Sattu,
+    },
+    {
+      title: "J.A.R.V.I.S",
+      icon: G1,
+    },
+    {
+      title: "J.A.R.V.I.S",
+      icon: Uj,
+    },
+    {
+      title: "Code Cipher",
+      icon: Ananya,
+    },
+    {
+      title: "Code Cipher",
+      icon: MahaL,
+    },
+    {
+      title: "Napster",
+      icon: Prer,
+    },
+    {
+      title: "Napster",
+      icon: Venki,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Workshops",
+      company_name: "BitsNbytes",
+      icon: BNB,
+      iconBg: "#383E56",
+      
+      points: [
+        "We regularly organize hands-on workshops that cover a wide range of technical topics.",
+        "Whether you're a beginner or a seasoned pro,",
+        "our workshops provide valuable insights and practical skills that can help you thrive in the tech industry.",
+      ],
+    },
+    {
+      title: "Gameathon",
+      company_name: "BitsNbytes",
+      
+      iconBg: "#E6DEDD",
+      date: "7th Dec 2023 - 8th Dec 2023",
+      points: [
+        " Our flagship event, Gamathon", 
+        "Is an internationally recognized gaming competition that brings together gaming enthusiasts from around the globe.",
+        "It's a showcase of talent, creativity, and competitive spirit that pushes the boundaries of gaming.",
+      ],
+    },
+    {
+      title: "Project Exhibition",
+      company_name: "BitsNbytes",
+      icon: BNB,
+      iconBg: "#383E56",
+      date: "Jan 2022 - Jan 2023",
+      points: [
+        " At our Project Exhibition events,",
+        " you can witness the innovative projects developed by our talented members.", 
+        "It's a platform for showcasing the latest technological advancements and inspiring others to create their own solutions.",
+      ],
+    },
+    {
+      title: "Web Development",
+      company_name: "BitsNBytes",
+      
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - Present",
+      points: [
+        "Dive into the world of web development with our Web Development Day.",
+        "From HTML and CSS basics to the latest web technologies,",
+        "we offer hands-on sessions to help you become a proficient web developer.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+      designation: "F.R.I.D.A.Y (LEAD HEAD)",
+      company: "BitsNBytes",
+      
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Web Development",
+      description:
+      "Dive into the world of web development with our Web Development Day. From HTML and CSS basics to the latest web technologies, we offer hands-on sessions to help you become a proficient web developer.",
+      tags: [
+        
+      ],
+      image: WeBD,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Gameathon",
+      description:
+        "Our flagship event, Gamathon, is an internationally recognized gaming competition that brings together gaming enthusiasts from around the globe. It's a showcase of talent, creativity, and competitive spirit that pushes the boundaries of gaming.",
+      tags: [
+        
+      ],
+      image: Game,
+      source_code_link: "https://gameathon-official-website.netlify.app/",
+    },
+    
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
